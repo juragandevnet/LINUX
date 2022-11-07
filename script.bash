daea9f516d0e763c25b10991040ba3a666561c98
juragandevnet <harrygunawanwijaya@gmail.com>
2022-11-07 12:59:37 +0800

    Update: Collect logs after run commands

--- script.bash
+++ script.bash
@@ -2,10 +2,12 @@
 
 set username "admin"
 set hostname "10.66.49.197"
+set password "cisco123"
 
 spawn ssh $username@$hostname
 expect "*assword: "
-send "cisco123\r"
+send "$password\r"
+log_file -noappend show_run.log
 expect "$ "
 send "terminal length 0\r"
 send "show run\r"
