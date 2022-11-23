20d89fbcbe2ab5a44db5d6ab24cc384af0ac162e
juragandevnet <harrygunawanwijaya@gmail.com>
2022-11-23 14:15:05 +0800

    CollectBackup.sh v1.0

--- CollectBackup_With_Bash_Scripting/collectbackup.sh
+++ CollectBackup_With_Bash_Scripting/collectbackup.sh
@@ -2,7 +2,7 @@
 
 set username "developer"
 set password "C1sco12345"
-set devices [open "devicelists.txt"]
+set devices [open "/root/devicelists.txt"]
 set date [clock format [clock seconds] -format {%d%m%Y-%T}]
 
 while {[gets $devices device] !=-1} {
@@ -12,11 +12,9 @@ while {[gets $devices device] !=-1} {
         log_file -noappend $device-$date.log
         expect "$ "
         send "terminal length 0\n"
-        send "show run | i hostname\n"
-        puts "show clock\n"
+        send "show run\n"
         expect "$ "
         send "exit\r"
         log_file
-        interact
         }
 close $devices
